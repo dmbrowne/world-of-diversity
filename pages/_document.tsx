09a1f51e8926493869261a2548427663a6f53a85
Daryl Browne <daryl.browne@googlemail.com>
2020-07-18 14:43:09 +0100

    feat(pages/_document): enable SSR for material-ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
 import Document, { DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets as MaterialStyleSheets } from "@material-ui/core/styles";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
+    const materialSheets = new MaterialStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(materialSheets.collect(<App {...props} />)),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -19,6 +21,7 @@ export default class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            {materialSheets.getStyleElement()}
           </>
         ),
       };
