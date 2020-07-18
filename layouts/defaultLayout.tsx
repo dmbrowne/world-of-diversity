8b594ad3082d64a6528a41e21df33b11f7947179
Daryl Browne <daryl.browne@googlemail.com>
2020-07-18 14:47:39 +0100

    refactor(layouts/default): using majority of logic from baseLayout

--- layouts/defaultLayout.tsx
+++ layouts/defaultLayout.tsx
@@ -1,35 +1,20 @@
-import React from "react";
-import Head from "next/head";
-import { ThemeProvider } from "styled-components";
-import Navbar from "../components/navbar/navbar";
-import { Grommet } from "grommet";
-import theme, { diversityTheme } from "../theme/diversity-theme";
+import { FC } from "react";
+import { Box } from "grommet";
+import Navbar from "@components/navbar";
+import Footer from "@components/footer";
+import BaseLayout from "./base";
 
-const DefaultLayout = ({ children }) => {
+const DefaultLayout: FC = ({ children }) => {
   return (
-    <ThemeProvider theme={diversityTheme}>
-      <Grommet theme={theme}>
-        <div className="container" style={{ minHeight: "100vh" }}>
-          <Head>
-            <title>Welcome to diversity</title>
-            <link
-              href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300;400;900&family=Rammetto+One&display=swap"
-              rel="stylesheet"
-            ></link>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
+    <BaseLayout title="Welcome to diversity">
+      <Box className="container" style={{ minHeight: "100vh" }}>
+        <Box flex={{ grow: 1 }}>
           <Navbar />
           {children}
-          <style jsx global>{`
-            html,
-            body {
-              padding: 0;
-              margin: 0;
-            }
-          `}</style>
-        </div>
-      </Grommet>
-    </ThemeProvider>
+        </Box>
+        <Footer />
+      </Box>
+    </BaseLayout>
   );
 };
 
